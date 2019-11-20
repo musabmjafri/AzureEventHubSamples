b70eb267f48a9aeca4c9453099d703a88824e7dc
Musab M. Jafri <musabmjafri@gmail.com>
2019-11-20 08:39:42 -0600

    Adding Sample Sender and Receiver

---
+++ SampleEphReceiver/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace SampleEphReceiver
+{
+    using System;
+    using System.Threading.Tasks;
+    using Microsoft.Azure.EventHubs;
+    using Microsoft.Azure.EventHubs.Processor;
+
+    public class Program
+    {
+        private const string EventHubConnectionString = "eventhubconnectionstring";
+        private const string EventHubName = "eventhubname";
+        private const string StorageContainerName = "storagecontainername";
+        private const string StorageAccountName = "storageaccountname";
+        private const string StorageAccountKey = "storageaccountkey";
+		
+        private static readonly string StorageConnectionString = string.Format("storageconnectionstring", StorageAccountName, StorageAccountKey);
+
+        public static void Main(string[] args)
+        {
+            MainAsync(args).GetAwaiter().GetResult();
+        }
+
+        private static async Task MainAsync(string[] args)
+        {
+            Console.WriteLine("Registering EventProcessor...");
+
+            var eventProcessorHost = new EventProcessorHost(
+                EventHubName,
+                PartitionReceiver.DefaultConsumerGroupName,
+                EventHubConnectionString,
+                StorageConnectionString,
+                StorageContainerName);
+
+            // Registers the Event Processor Host and starts receiving messages
+            await eventProcessorHost.RegisterEventProcessorAsync<SimpleEventProcessor>();
+
+            Console.WriteLine("Receiving. Press enter key to stop worker.");
+            Console.ReadLine();
+
+            // Disposes of the Event Processor Host
+            await eventProcessorHost.UnregisterEventProcessorAsync();
+        }
+    }
+}
---
+++ SampleSender/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace SampleSender
+{
+    using System;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Microsoft.Azure.EventHubs;
+
+    public class Program
+    {
+        private static EventHubClient eventHubClient;
+        private const string EventHubConnectionString = "eventhubconnectionstring";
+        private const string EventHubName = "eventhubname";
+
+        public static void Main(string[] args)
+        {
+            MainAsync(args).GetAwaiter().GetResult();
+        }
+
+        private static async Task MainAsync(string[] args)
+        {
+            // Creates an EventHubsConnectionStringBuilder object from a the connection string, and sets the EntityPath.
+            // Typically the connection string should have the Entity Path in it, but for the sake of this simple scenario
+            // we are using the connection string from the namespace.
+            var connectionStringBuilder = new EventHubsConnectionStringBuilder(EventHubConnectionString)
+            {
+                EntityPath = EventHubName
+            };
+
+            eventHubClient = EventHubClient.CreateFromConnectionString(connectionStringBuilder.ToString());
+
+            Console.WriteLine("Enter number of messages to stream: ");
+            string messageCount = Console.ReadLine();
+            int count = 1;
+
+            try
+            {
+                count = Convert.ToInt32(messageCount);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"{DateTime.Now} > Exception: {exception.Message}");
+            }
+
+            await SendMessagesToEventHub(count);
+
+            await eventHubClient.CloseAsync();
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadLine();
+        }
+
+        // Creates an Event Hub client and sends 100 messages to the event hub.
+        private static async Task SendMessagesToEventHub(int numMessagesToSend)
+        {
+            for (var i = 0; i < numMessagesToSend; i++)
+            {
+                try
+                {
+                    var message = "message";
+
+                    await eventHubClient.SendAsync(new EventData(Encoding.UTF8.GetBytes(message)));
+
+                    Console.WriteLine($"Message {i} sent: {message}");
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine($"{DateTime.Now} > Exception: {exception.Message}");
+                }
+
+                await Task.Delay(1000);
+            }
+
+            Console.WriteLine($"{numMessagesToSend} messages sent.");
+        }
+    }
+}
